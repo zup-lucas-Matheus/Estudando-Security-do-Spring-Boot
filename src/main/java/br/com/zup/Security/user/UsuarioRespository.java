9b16ba4235e7b8ef2a00910264bf0b1443da3190
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-09-08 13:53:49 -0300

    Adicionar repository para usuario.

---
+++ src/main/java/br/com/zup/Security/user/UsuarioRespository.java
@@ -0,0 +1,9 @@
+package br.com.zup.Security.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRespository extends CrudRepository<Usuario, Integer> {
+
+
+
+}
